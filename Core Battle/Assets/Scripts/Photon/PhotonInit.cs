ab3a94e92d4d89c49261f39017f0f6a79c07dd42
20201406최수지 <83389459+tnwdl@users.noreply.github.com>
2022-12-13 07:00:15 +0900

    Server
    End

--- Core Battle/Assets/Scripts/Photon/PhotonInit.cs
+++ Core Battle/Assets/Scripts/Photon/PhotonInit.cs
@@ -7,6 +7,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Data;
 using UnityEngine.Networking.PlayerConnection;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 
 public class PhotonInit : MonoBehaviourPunCallbacks
@@ -16,15 +17,31 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     public GameObject LobbyPanel;
     public GameObject MakeRoomPanel;
     public GameObject RoomPanel;
+    public GameObject PwPanel;
+    public GameObject PwErrorLog;
+
     public InputField PlayerInput;
     public InputField RoomInput;
+    public InputField RoomPwInput;
+    public InputField PwCheckIF;
+
+    public Toggle PwToggle;
+
+    public Button[] CellBtn;
+    public Button PreviousBtn;
+    public Button NextBtn;
+    public Button CreateRoomBtn;
 
     public string gameVersion = "CoreBattle 1.0";
     public string playerID = "";
+    public int hashtablecount;
 
     private bool isGameStart = false;
     private bool isLogin = false;
     private string connectionState = "";
+
+    List<RoomInfo> myList = new List<RoomInfo>();
+    private int curPage = 1, maxPage, multiple, roomNum;
 
     Text connectionInfo;
 
@@ -32,11 +49,11 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     {
         get
         {
-            if(!instance)
+            if (!instance)
             {
                 instance = FindObjectOfType(typeof(PhotonInit)) as PhotonInit;
 
-                if(instance == null)
+                if (instance == null)
                 {
                     Debug.Log("no singleton object");
                 }
@@ -56,7 +73,7 @@ public class PhotonInit : MonoBehaviourPunCallbacks
 
             OnLogin();
         }
-        else if(isGameStart == true && isLogin == true)
+        else if (isGameStart == true && isLogin == true)
         {
             PlayerInput.text = string.Empty;
         }
@@ -87,6 +104,83 @@ public class PhotonInit : MonoBehaviourPunCallbacks
 
         isGameStart = false;
         isLogin = false;
+    }
+    public void CreatNewRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.CustomRoomProperties = new Hashtable()
+        {
+            {"password", RoomPwInput.text }
+        };
+        roomOptions.CustomRoomPropertiesForLobby = new string[] { "password" };
+
+        if (PwToggle.isOn)
+        {
+            PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Game" + Random.Range(0, 100) : RoomInput.text, roomOptions);
+        }
+        else
+        {
+            PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Game" + Random.Range(0, 100) : RoomInput.text, new RoomOptions { MaxPlayers = 10 });
+        }
+
+        MakeRoomPanel.SetActive(false);
+    }
+    public void MyListClick(int num)
+    {
+        if (num == -2)
+        {
+            --curPage;
+            MyListRenewal();
+        }
+        else if (num == -1)
+        {
+            ++curPage;
+            MyListRenewal();
+        }
+        else if (myList[multiple + num].CustomProperties["password"] != null)
+        {
+            PwPanel.SetActive(true);
+        }
+        else
+        {
+            PhotonNetwork.JoinRoom(myList[multiple + num].Name);
+            MyListRenewal();
+        }
+    }
+    public void RoomPw(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                roomNum = 0;
+                break;
+            case 1:
+                roomNum = 1;
+                break;
+            case 2:
+                roomNum = 2;
+                break;
+            case 3:
+                roomNum = 3;
+                break;
+
+            default:
+                break;
+        }
+    }
+    public void EnterRoomWithPW()
+    {
+        if ((string)myList[multiple + roomNum].CustomProperties["password"] == PwCheckIF.text)
+        {
+            PhotonNetwork.JoinRoom(myList[multiple + roomNum].Name);
+            MyListRenewal();
+            PwPanel.SetActive(false);
+        }
+        else
+        {
+            StartCoroutine("ShowPwWrongMsg");
+        }
     }
 
     public override void OnJoinedLobby()
@@ -126,6 +220,7 @@ public class PhotonInit : MonoBehaviourPunCallbacks
             connectionInfo.text = connectionState;
 
         isLogin = true;
+        
         PhotonNetwork.LoadLevel("SampleScene");
     }
     public override void OnCreatedRoom()
@@ -135,6 +230,34 @@ public class PhotonInit : MonoBehaviourPunCallbacks
         connectionState = "·ë »ý¼º ¿Ï·á";
         if (connectionInfo)
             connectionInfo.text = connectionState;
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        Debug.Log("OnRoomListUpdate:" + roomList.Count);
+
+        int roomCount = roomList.Count;
+
+        for (int i = 0; i < roomCount; i++)
+        {
+            if (!roomList[i].RemovedFromList)
+            {
+                if (!myList.Contains(roomList[i]))
+                {
+                    myList.Add(roomList[i]);
+                }
+                else
+                {
+                    myList[myList.IndexOf(roomList[i])] = roomList[i];
+                }
+            }
+            else if (myList.IndexOf(roomList[i]) != -1)
+            {
+                myList.RemoveAt(myList.IndexOf(roomList[i]));
+            }
+        }
+
+        MyListRenewal();
     }
 
     private void OnLogin()
@@ -161,6 +284,24 @@ public class PhotonInit : MonoBehaviourPunCallbacks
             PhotonNetwork.ConnectUsingSettings();
         }
     }
+    private void MyListRenewal()
+    {
+        maxPage = (myList.Count % CellBtn.Length == 0)
+            ? myList.Count / CellBtn.Length
+            : myList.Count / CellBtn.Length + 1;
+
+        PreviousBtn.interactable = (curPage <= 1) ? false : true;
+        NextBtn.interactable = (curPage >= maxPage) ? false : true;
+
+        multiple = (curPage - 1) * CellBtn.Length;
+        for (int i = 0; i < CellBtn.Length; i++)
+        {
+            CellBtn[i].interactable = (multiple + i < myList.Count) ? true : false;
+            CellBtn[i].transform.GetChild(0).GetComponent<Text>().text = (multiple + i < myList.Count) ? myList[multiple + i].Name : "";
+            CellBtn[i].transform.GetChild(1).GetComponent<Text>().text = (multiple + i < myList.Count)
+             ? myList[multiple + i].PlayerCount + "/" + myList[multiple + i].MaxPlayers : "";
+        }
+    }
 
     private void Awake()
     {
@@ -182,10 +323,19 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     }
     private void Update()
     {
-        if(isGameStart == false && SceneManager.GetActiveScene().name == "SampleScene" && isLogin == true)
+        if (isGameStart == false && SceneManager.GetActiveScene().name == "SampleScene" && isLogin == true)
         {
             isGameStart = true;
         }
     }
 
+    IEnumerator ShowPwWrongMsg()
+    {
+        if (!PwErrorLog.activeSelf)
+        {
+            PwErrorLog.SetActive(true);
+            yield return new WaitForSeconds(3.0f);
+            PwErrorLog.SetActive(false);
+        }
+    }
 }
---
+++ Core Battle/Assets/Scripts/UI/LoadingSceneManager.cs
@@ -0,0 +1,58 @@
+using Palmmedia.ReportGenerator.Core.Reporting.Builders.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    public static string nextScene;
+
+    [SerializeField] 
+    Image progressBar;
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0.0f;
+
+        while (!op.isDone)
+        {
+            yield return null;
+
+            
+
+            if(op.progress < 0.9f)
+            {
+                progressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+
+                progressBar.fillAmount = Mathf.Lerp(0.9f, 1.0f, timer);
+
+                if(progressBar.fillAmount >= 1.0f)
+                {
+                    op.allowSceneActivation = true;
+
+                    yield break;
+                }
+            }
+        }
+    }
+}
