8097e620096a991480fe371708dfd15200e1860c
20201406최수지 <83389459+tnwdl@users.noreply.github.com>
2022-12-12 22:32:48 +0900

    hi
    포톤

--- Core Battle/Assets/Scripts/Photon/PhotonInit.cs
+++ Core Battle/Assets/Scripts/Photon/PhotonInit.cs
@@ -12,127 +12,32 @@ public class PhotonInit : MonoBehaviourPunCallbacks
 {
     public InputField playerInput;
 
-    public static PhotonInit instance;
+    public string gameVersion = "CoreBattle 1.0";
+    public string playerID = "";
 
-
-
-    private bool isGameStart = false;
-    private bool isLogin = false;
-    private string playerID = "";
-
-
-    //게임 시작 버튼
-    public void SetPlayerName()
+    public void SetPlayer()
     {
-        Debug.Log("플레이어 ID: " + playerInput.text);
-
-        if (isGameStart == false && isLogin == false)
-        {
-            playerID = playerInput.text;
-            playerInput.text = string.Empty;
-
-            Debug.Log("connect 시도" + isGameStart + ", " + isLogin);
-
-            Connect();
-        }
+        OnLogin();
     }
 
-
-    //로비 입장시 호출
-    public override void OnJoinedLobby()
+    public override void OnConnectedToMaster()
     {
-        Debug.Log("Join Lobby");
+        base.OnConnectedToMaster();
 
-        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Connected");
     }
 
-    //룸 입장 실패시 호출
-    public override void OnJoinRandomFailed(short returnCode, string message)
+    private void OnLogin()
     {
-        Debug.Log("No Room");
-    }
-
-    //룸 생성 완료시 호출
-    public override void OnCreatedRoom()
-    {
-        base.OnCreatedRoom();
-
-        Debug.Log("Finish make a room");
-    }
-
-    //룸 입장시 호출
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-
-        Debug.Log("Joined Room");
-        isLogin = true;
-
-        PlayerPrefs.SetInt("LogIn", 1);
-
-        PhotonNetwork.LoadLevel("demo");
-    }
-
-    public static PhotonInit Instance
-    {
-        get
-        {
-            if (!instance)
-            {
-                instance = FindObjectOfType(typeof(PhotonInit)) as PhotonInit;
-
-                if (instance == null)
-                {
-                    Debug.Log("No Singleton Obj");
-                }
-            }
-
-            return instance;
-        }
-    }
-
-
-    //룸 접속
-    public void Connect()
-    {
-        if(PhotonNetwork.IsConnected)
-        {
-            PhotonNetwork.JoinRandomRoom();
-        }
-        else
-        {
-            PhotonNetwork.ConnectUsingSettings();
-        }
-    }
-
-
-
-    private void Awake()
-    {
-        PhotonNetwork.GameVersion = "CoreBattle 1.0";
+        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.NickName = this.playerID;
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    private void Update()
+    private void Awake()
     {
-        if(PlayerPrefs.GetInt("LogIn") == 1)
-        {
-            isLogin = true;
-        }
-
-        if (isGameStart == false && SceneManager.GetActiveScene().name == "demo" && isLogin == true)
-        {
-            isGameStart = true;
-        }
-
-        StartCoroutine(CreatPlayer());
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
 
-    IEnumerator CreatPlayer()
-    {
-        //PhotonNetwork.Instantiate("Player", new Vector3(0, 1, 0), Quaternion.identity, 0);
-
-        yield return null;
-    }
 }
